6f5cce5f41cb3366af23b2596ec13aecfd17ae7b
Jupiter <jupiterbaudot@gmail.com>
2024-06-19 18:46:12 -0700

    CHATROOM: sets up basic chat room component with pubnub

--- src/components/ChatRoom.tsx
+++ src/components/ChatRoom.tsx
@@ -1,25 +1,47 @@
 import React, { useEffect, useState } from 'react';
 import PubNub from 'pubnub';
-import { PubNubProvider } from 'pubnub-react';
+import { usePubNub } from 'pubnub-react';
 
+interface Message {
+    id: string;
+    text: string;
+    senderId: string;
+    timestamp: Date;
+}
 
 const ChatRoom = () => {
-    const [messages, setMessages] = useState([]);
+    const pubnub = usePubNub();
+    const [messages, setMessages] = useState<Message[]>([]);
+    const [channel] = useState<string>('yellowBirdChat');
 
     useEffect(() => {
-        // const pubnub = new PubNub({
-        //     publishKey: 
-        //     subscribeKey: 
-        // });
+        const handleMessage = (event: PubNub.MessageEvent) => {
+            const newMessage: Message = {
+                id: event.message.id,
+                text: event.message.text,
+                senderId: event.message.senderId,
+                timestamp: new Date(Number(event.timetoken) / 10000)
+            };
+            setMessages(prevMessages => [...prevMessages, newMessage]);
+        };
 
-        const currentChannel = "Default";
-        const theme = "light";
-    }, []);
+        pubnub.addListener({ message: handleMessage });
+        pubnub.subscribe({ channels: [channel] });
+
+        return () => pubnub.unsubscribeAll();
+    }, [pubnub, channel]);
 
     return (
         <div>
-            {/* MessageList and ChatInput components will go here */}
-            <div>Chat room component</div>
+            <h1>Chat Room</h1>
+            <ul>
+                {messages.map((message) => (
+                    <li key={message.id}>
+                        {message.senderId}: {message.text} ({message.timestamp.toLocaleTimeString()})
+                    </li>
+                ))}
+            </ul>
+        
         </div>
     );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PubNub from 'pubnub';
+import { PubNubProvider } from 'pubnub-react';
 import reportWebVitals from './reportWebVitals';
+
+const pubnub = new PubNub({
+  publishKey: 'pub-c-75350107-b960-494a-8f19-dd1e98236eef',
+  subscribeKey: 'sub-c-70e805dd-6ce9-419c-aed6-9873b4fed81a',
+  userId: '001' 
+});
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <PubNubProvider client={pubnub}>
+        <App />
+    </PubNubProvider>
   </React.StrictMode>
 );
 
