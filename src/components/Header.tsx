1fe403461fc68850da01e8b5e067df1900ded967
Jupiter <jupiterbaudot@gmail.com>
2024-06-28 12:31:01 -0700

    sessions: refactor sessions so that there is only leave session and delete end session and isCreator

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,30 +3,19 @@ import '../styles/header.css';
 
 interface HeaderProps {
     channel: string;
-    isCreator: boolean;
-    onEndSession: () => void;
     onLeaveSession: () => void;
 }
 
-const Header: React.FC<HeaderProps> = ({ channel, isCreator, onEndSession, onLeaveSession }) => {
+const Header: React.FC<HeaderProps> = ({ channel, onLeaveSession }) => {
     return (
         <div className="header">
             <h1 className="title">Yellow Bird Chatter</h1>
             <div className="room-code">Room Code: {channel}</div>
-            {isCreator ? (
-                <button className="end-session-button" onClick={onEndSession}>End Session</button>
-            ) : (
-                <button className="leave-session-button" onClick={onLeaveSession}>Leave Session</button>
-            )}
+
+            <button className="leave-session-button" onClick={onLeaveSession}>Leave Session</button>
+            
         </div>
     );
 };
 
 export default Header;
-
-
-// {channel && (isCreator ? (
-//     <button className="end-session-button" onClick={onEndSession}>End Session</button>
-// ) : (
-//     <button className="leave-session-button" onClick={onLeaveSession}>Leave Session</button>
-// ))}
